cf5b2b0fbb69fee0cd4d774cee373634546ee9aa
ByeongJun <neostgeart@ajou.ac.kr>
2021-08-22 04:41:14 +0900

    Schedule Front Complete

--- front/pages/Schedule.js
+++ front/pages/Schedule.js
@@ -4,6 +4,7 @@ import 'date-fns';
 import React, { useCallback, useState } from 'react';
 import Grid from '@material-ui/core/Grid';
 import DateFnsUtils from '@date-io/date-fns';
+import { alpha } from '@material-ui/core/styles';
 import {
     MuiPickersUtilsProvider,
     KeyboardTimePicker,
@@ -14,30 +15,29 @@ import AddCircleIcon from '@material-ui/icons/AddCircle';
 import IndeterminateCheckBoxIcon from '@material-ui/icons/IndeterminateCheckBox';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import useInput from '../hooks/useInput';
 
 const Schedule = () => {
     const [selectedDate, setSelectedDate] = useState(new Date());
-    const [countList, setCountList] = useState([])
+    const [input1,onChangeInput1] = useInput('');
+    const [input2,onChangeInput2] = useInput('');
+    const [input3,onChangeInput3] = useInput('');
+    const [input4,onChangeInput4] = useInput('');
+    const [input5,onChangeInput5] = useInput('');
+
     const handleDateChange = useCallback((date) => {
         setSelectedDate(date);
+        console.log(date);
     },[selectedDate]);
 
-    const clickadd = useCallback(()=>{
-        let countArr = [...countList]
-        let counter = countArr.slice(-1)[0]
-        counter += 1
-        countArr.push(counter)	// index 사용 X
-        // countArr[counter] = counter	// index 사용 시 윗줄 대신 사용	
-        setCountList(countArr)
-    },[countList]);
 
-
-    const clickremove = useCallback(()=>{
-        let countArr = [...countList]
-        countArr.pop()	// index 사용 X
-        // countArr[counter] = counter	// index 사용 시 윗줄 대신 사용	
-        setCountList(countArr)
-    },[countList]);
+    const onClickSubmit = useCallback(()=>{
+        console.log(input1);
+        console.log(input2);
+        console.log(input3);
+        console.log(input4);
+        console.log(input5);
+    },[input1,input2,input3,input4,input5]);
 
     return (
         <>
@@ -58,17 +58,13 @@ const Schedule = () => {
                     />
                     </Grid>
                 </MuiPickersUtilsProvider>
+                <div><TextField label="일정1" style = {{marginBottom : "15px", marginTop : "5px"}} value = {input1} onChange = {onChangeInput1}></TextField></div>
+                <div><TextField label="일정2" style = {{marginBottom : "15px", marginTop : "5px"}} value = {input2} onChange = {onChangeInput2}></TextField></div>
+                <div><TextField label="일정3" style = {{marginBottom : "15px", marginTop : "5px"}} value = {input3} onChange = {onChangeInput3}></TextField></div>
+                <div><TextField label="일정4" style = {{marginBottom : "15px", marginTop : "5px"}} value = {input4} onChange = {onChangeInput4}></TextField></div>
+                <div><TextField label="일정5" style = {{marginBottom : "15px", marginTop : "5px"}} value = {input5} onChange = {onChangeInput5}></TextField></div>
                 <div>
-                    <IconButton color="primary" aria-label="add schedule" onClick = {clickadd}>
-                        <AddCircleIcon />
-                    </IconButton>
-                    <IconButton color="primary" aria-label="remove schedule" onClick = {clickremove}>
-                        <IndeterminateCheckBoxIcon />
-                    </IconButton>
-                </div>
-                {countList && countList.map((v,i) => (<div><TextField label="일정" key = {i} style = {{marginBottom : "15px", marginTop : "5px"}}></TextField></div>))}
-                <div>
-                <Button variant="outlined" color="primary" style = {{marginTop:"10px"}}>
+                <Button variant="outlined" color="primary" style = {{marginTop:"10px"}} onClick = {onClickSubmit}>
                     설정 완료
                 </Button>
                 </div>
