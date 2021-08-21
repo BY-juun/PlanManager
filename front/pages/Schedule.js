ab3d8fc3109da3c64f206809f644e45c80919269
ByeongJun <neostgeart@ajou.ac.kr>
2021-08-22 01:52:01 +0900

    Schedule page

--- front/pages/Schedule.js
+++ front/pages/Schedule.js
@@ -1,7 +1,7 @@
 import TopLayout from '../components/TopLayout';
 import BottomLayout from '../components/BottomLayout';
 import 'date-fns';
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import Grid from '@material-ui/core/Grid';
 import DateFnsUtils from '@date-io/date-fns';
 import {
@@ -9,19 +9,40 @@ import {
     KeyboardTimePicker,
     KeyboardDatePicker,
 } from '@material-ui/pickers';
-
+import IconButton from '@material-ui/core/IconButton';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import IndeterminateCheckBoxIcon from '@material-ui/icons/IndeterminateCheckBox';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 const Schedule = () => {
     const [selectedDate, setSelectedDate] = useState(new Date());
-
-    const handleDateChange = (date) => {
+    const [countList, setCountList] = useState([])
+    const handleDateChange = useCallback((date) => {
         setSelectedDate(date);
-    };
+    },[selectedDate]);
+
+    const clickadd = useCallback(()=>{
+        let countArr = [...countList]
+        let counter = countArr.slice(-1)[0]
+        counter += 1
+        countArr.push(counter)	// index 사용 X
+        // countArr[counter] = counter	// index 사용 시 윗줄 대신 사용	
+        setCountList(countArr)
+    },[countList]);
+
+
+    const clickremove = useCallback(()=>{
+        let countArr = [...countList]
+        countArr.pop()	// index 사용 X
+        // countArr[counter] = counter	// index 사용 시 윗줄 대신 사용	
+        setCountList(countArr)
+    },[countList]);
 
     return (
         <>
             <TopLayout></TopLayout>
-            <div>
+            <div style = {{textAlign : "center", marginTop : "60px"}}>
                 <MuiPickersUtilsProvider utils={DateFnsUtils}>
                     <Grid container justifyContent="space-around">
                     <KeyboardDatePicker
@@ -37,8 +58,21 @@ const Schedule = () => {
                     />
                     </Grid>
                 </MuiPickersUtilsProvider>
+                <div>
+                    <IconButton color="primary" aria-label="add schedule" onClick = {clickadd}>
+                        <AddCircleIcon />
+                    </IconButton>
+                    <IconButton color="primary" aria-label="remove schedule" onClick = {clickremove}>
+                        <IndeterminateCheckBoxIcon />
+                    </IconButton>
+                </div>
+                {countList && countList.map((v,i) => (<div><TextField label="일정" key = {i} style = {{marginBottom : "15px", marginTop : "5px"}}></TextField></div>))}
+                <div>
+                <Button variant="outlined" color="primary" style = {{marginTop:"10px"}}>
+                    설정 완료
+                </Button>
+                </div>
             </div>
-
             <BottomLayout></BottomLayout>
         </>
     );
